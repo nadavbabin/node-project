4546b6df194f89fd3178448f5d440902d28fd7db
nadav <nadav.babin@gmail.com>
2018-02-17 19:41:30 +0200

    fuck push

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 //dd
 app.get('/',function(req,res) {
-  res.send("Hello World");
+  res.send("Hello World bitches!!");
 });
 
 app.listen(4000);
